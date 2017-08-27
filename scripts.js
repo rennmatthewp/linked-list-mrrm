54e7dc78021945fb3d73e1110334dc89a7550f77
Matthew Renn <rennmatthewp@gmail.com>
2017-08-26 18:04:57 -0600

    bumble around a bit

--- scripts.js
+++ scripts.js
@@ -4,12 +4,17 @@ function fillCard() {
 	event.preventDefault();
 	var title = document.querySelector('#input-title');
 	var url = document.querySelector('#input-url');
-	var bookmarkTitle = document.querySelector('#bookmark-title');
-	var bookmarkURL = document.querySelector('#bookmark-url');
+	var bookmarkTitle = document.querySelector('.bookmark-title');
+	var bookmarkURL = document.querySelector('.bookmark-url');
 
-	bookmarkURL.href = 'http://' + url.value;
 	bookmarkTitle.innerText = title.value;
-	bookmarkURL.innerText = url.value;
+	bookmarkURL.innerHTML = url.value;
+	// bookmarkURL.setAttribute('href', 'http://' + url.value)
+	if (bookmarkURL.href.startsWith('http') === true) {
+		bookmarkURL.href = url.value
+	} else {bookmarkURL.href = 'http://' + url.value;
+};
+console.log(bookmarkURL.href);
 }
 
 function newCard() {
@@ -19,22 +24,10 @@ function newCard() {
 	bookmarkCard.classList.add('bookmark-card')
 	bookmarkCard.innerHTML = document.querySelector('.bookmark-card').innerHTML;
 	section.appendChild(bookmarkCard);
-
-
-
-
-
-
-	// var cardHolder = document.querySelector('#right');
-	// var bookmarkCard = document.querySelector('.bookmark-card')
-	// bookmarkCard = document.createElement('article');
-	// bookmarkCard.textContent = bookmarkCard;
-	// cardHolder.appendChild(bookmarkCard);
-	// console.log(bookmarkCard);
 }
 
 //Event Listeners
 
 document.querySelector('#input-submit').addEventListener('click', function() {
-	newCard();
+	fillCard();
 });
