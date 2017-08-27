f3c685e2afbc0fa8c4ab569124a03155a9bbc0fc
Rob Morgan <rmorgan323@gmail.com>
2017-08-26 22:22:08 -0600

    fix form and js logic

--- scripts.js
+++ scripts.js
@@ -1,33 +1,36 @@
 //Functions
 
-function fillCard() {
-	event.preventDefault();
+function fillCard(bookmarkCardObject) {
+	// event.preventDefault();
 	var title = document.querySelector('#input-title');
 	var url = document.querySelector('#input-url');
-	var bookmarkTitle = document.querySelector('.bookmark-title');
-	var bookmarkURL = document.querySelector('.bookmark-url');
+	var bookmarkTitle = bookmarkCardObject.querySelector('.bookmark-title');
+	var bookmarkURL = bookmarkCardObject.querySelector('.bookmark-url');
 
 	bookmarkTitle.innerText = title.value;
 	bookmarkURL.innerHTML = url.value;
 	// bookmarkURL.setAttribute('href', 'http://' + url.value)
-	if (bookmarkURL.href.startsWith('http') === true) {
-		bookmarkURL.href = url.value
-	} else {bookmarkURL.href = 'http://' + url.value;
-};
-console.log(bookmarkURL.href);
+// 	if (bookmarkURL.href.startsWith('http') === true) {
+// 		bookmarkURL.href = url.value
+// 	} else {bookmarkURL.href = 'http://' + url.value;
+// };
+// console.log(bookmarkURL.href);
 }
 
 function newCard() {
-	event.preventDefault();
 	var section = document.querySelector('#right')
 	var bookmarkCard = document.createElement('article');
 	bookmarkCard.classList.add('bookmark-card')
+	bookmarkCard.style.setProperty('display', 'block')
 	bookmarkCard.innerHTML = document.querySelector('.bookmark-card').innerHTML;
+	fillCard(bookmarkCard);
 	section.appendChild(bookmarkCard);
 }
 
 //Event Listeners
 
-document.querySelector('#input-submit').addEventListener('click', function() {
-	fillCard();
+document.querySelector('#input-form').addEventListener('submit', function() {
+	event.preventDefault();
+	newCard();
 });
+
