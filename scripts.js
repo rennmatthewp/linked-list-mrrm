d2f3bb28f715dc5d372354bc529adf5fe8519671
Rob Morgan <rmorgan323@gmail.com>
2017-08-29 12:27:54 -0600

    css transitions and animations

--- scripts.js
+++ scripts.js
@@ -11,14 +11,26 @@ function fillCard(bookmarkCardObject) {
 }
 
 function newCard() {
-	var section = document.querySelector('#right')
+	var section = document.querySelector('#right');
+	var left = document.querySelector('.left');
+	var homestar = document.querySelector('.homestar');
 	var bookmarkCard = document.createElement('article');
 	bookmarkCard.classList.add('bookmark-card')
 	bookmarkCard.style.setProperty('display', 'block')
 	bookmarkCard.innerHTML = document.querySelector('.bookmark-card').innerHTML;
 	fillCard(bookmarkCard);
+
+
+	homestar.classList.remove('homestar-onload');
+	left.classList.remove('left-onload');
+
+
 	section.appendChild(bookmarkCard);
 
+	if (document.querySelectorAll('.bookmark-card').length < 3) {
+
+	setTimeout(function() {
+		
 	if (document.querySelectorAll('.bookmark-card').length > 2) {
 		setTimeout(function() {
 			bookmarkCard.classList.add('bookmark-fade');
@@ -27,8 +39,26 @@ function newCard() {
 		section.classList.add('bookmark-fade');
 		setTimeout(function() {
 			bookmarkCard.classList.add('bookmark-fade');
-		}, 500)
+
+		}, 200)
 	}
+
+	}, 500)
+
+	} else {
+		if (document.querySelectorAll('.bookmark-card').length > 2) {
+		setTimeout(function() {
+			bookmarkCard.classList.add('bookmark-fade');
+		}, 10)
+	} else {
+		section.classList.add('bookmark-fade');
+		setTimeout(function() {
+			bookmarkCard.classList.add('bookmark-fade');
+
+		}, 200)
+	}
+	}
+
 }
 
 function enableButton() {
@@ -58,15 +88,27 @@ document.querySelector('#input-form').addEventListener('submit', function(e) {
 	enableButton();
 });
 
+
+
+
 document.querySelector('#right').addEventListener('click', function(event) {
 	var section = document.querySelector('#right');
+	var left = document.querySelector('.left');
+	var homestar = document.querySelector('.homestar');
 
 	if (event.target.matches('h5.delete-button')) {
+
 		event.target.parentNode.classList.remove('bookmark-fade');
 		setTimeout(function() {
 			event.target.parentNode.remove();
 			if (document.querySelectorAll('.bookmark-card').length < 2) {
 				section.classList.remove('bookmark-fade');
+
+				homestar.classList.add('homestar-onload');
+				left.classList.add('left-onload');
+
+
+
 			}
 		}, 800)
 	}
